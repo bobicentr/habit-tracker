477b6f21e3fd9d80d4f35ef60dff15b2e2f9a946
dan <wannabe00@mail.ru>
2025-12-23 20:27:03 +0100

    deploy tweaks

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/habit-tracker/'
+  base: '/habit-tracker/',
   plugins: [react(), tailwindcss()],
 })
